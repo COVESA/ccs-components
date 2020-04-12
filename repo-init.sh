4dcc6300ba004797e656ce069e90dd2c6501c6b7
Sanjeev BA <iamsanjeev@gmail.com>
2020-04-12 11:22:14 +0900

    Force override in script.
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>

--- repo-init.sh
+++ repo-init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git submodule add -b master https://github.com/MEAE-GOT/W3C_VehicleSignalInterfaceImpl.git
+git submodule add --force -b master https://github.com/MEAE-GOT/W3C_VehicleSignalInterfaceImpl.git
 git submodule init
 git submodule update --remote
 
