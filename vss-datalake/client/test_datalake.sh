4b1593a4e4e84cf300aedfdd98f35761d6006ad2
Sanjeev BA <iamsanjeev@gmail.com>
2020-05-06 21:35:02 +0900

    Indentation fixes.
    Signed-off-by: Sanjeev BA <iamsanjeev@gmail.com>

--- vss-datalake/client/test_datalake.sh
+++ vss-datalake/client/test_datalake.sh
@@ -9,30 +9,30 @@ ct='Content-Type:application/json'
 set_example()
 {
 
-	echo '{"action":"set", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "true", "timestamp":"2020-01-10T02:59:43.492Z"}'
+	echo '{"action":"set", "vin": "GEO001", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "true", "timestamp":"2020-01-10T02:59:43.492Z"}'
 	curl -d \
-	'{"action":"set", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "true", "timestamp":"2020-01-10T02:59:43.492Z"}' \
+	'{"action":"set", "vin": "GEO001", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "true", "timestamp":"2020-01-10T02:59:43.492Z"}' \
 	-H "$ct" "$serverurl"
 	sleep 1
 	echo ""
 
-	echo '{"action":"set", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "false", "timestamp":"2020-01-11T02:59:43.492Z"}'
+	echo '{"action":"set", "vin": "GEO001", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "false", "timestamp":"2020-01-11T02:59:43.492Z"}'
 	curl -d \
-	'{"action":"set", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "false", "timestamp":"2020-01-11T02:59:43.492Z"}' \
+	'{"action":"set", "vin": "GEO001", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "false", "timestamp":"2020-01-11T02:59:43.492Z"}' \
 	-H $ct $serverurl
 	sleep 1
 	echo ""
 
-	echo '{"action":"set", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "true", "timestamp":"2020-01-12T02:59:43.492Z"}'
+	echo '{"action":"set", "vin": "GEO001", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "true", "timestamp":"2020-01-12T02:59:43.492Z"}'
 	curl -d \
-	'{"action":"set", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "true", "timestamp":"2020-01-12T02:59:43.492Z"}' \
+	'{"action":"set", "vin": "GEO001", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "true", "timestamp":"2020-01-12T02:59:43.492Z"}' \
 	-H $ct $serverurl
 	sleep 1
 	echo ""
 
-	echo '{"action":"set", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "false", "timestamp":"2020-04-01T02:59:43.492Z"}'
+	echo '{"action":"set", "vin": "GEO001", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "false", "timestamp":"2020-04-01T02:59:43.492Z"}'
 	curl -d \
-	'{"action":"set", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "false", "timestamp":"2020-04-01T02:59:43.492Z"}' \
+	'{"action":"set", "vin": "GEO001", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen", "value": "false", "timestamp":"2020-04-01T02:59:43.492Z"}' \
 	-H $ct $serverurl
 	sleep 1
 	echo ""
@@ -55,30 +55,31 @@ get_example()
 	sleep 1
 	echo ""
 
-	echo '{"action":"get", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen"}'
+	echo '{"action":"get", "vin": "GEO001", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen"}'
 	curl -d \
-	'{"action":"get", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen"}' \
+	'{"action":"get", "vin": "GEO001", "path":"Vehicle/Cabin/Door/Row1/Left/IsOpen"}' \
 	-H $ct $serverurl
 	sleep 1
 	echo ""
 
-	echo '{action:get, vin: YV1DZ8256C2271234, path:Vehicle/Cabin/Door/Row1/Left/IsOpen}'
-	curl -d '{action:get, vin: YV1DZ8256C2271234, path:Vehicle/Cabin/Door/Row1/Left/IsOpen}' \
+	echo '{action:get, vin: GEO001, path:Vehicle/Cabin/Door/Row1/Left/IsOpen}'
+	curl -d '{action:get, vin: GEO001, path:Vehicle/Cabin/Door/Row1/Left/IsOpen}' \
 	-H $ct $serverurl
 	echo ""
 }
 
-get_example
-get_example
-get_example
-get_example
-get_example
+set_example
+#get_example
+#get_example
+#get_example
+#get_example
+#get_example
 
 
 #**** GET response examples ****
 
 #curl -d \
-#'{"action":"getmetadata", "vin": "YV1DZ8256C2271234", "path":"Vehicle/Cabin/Door/", "depth": "2"}'\
+#'{"action":"getmetadata", "vin": "GEO001", "path":"Vehicle/Cabin/Door/", "depth": "2"}'\
 #-H 'Content-Type: application/json' http://localhost:8765/datalakeserver
 #{"datapackage":"{ \"path\":\"Vehicle/Cabin/Door/Row1/Left/IsOpen, \"datapoints\":\"{\"value\": \"false\", \"timestamp\": \"2020-04-01T02:59:43.492Z\"}}"}
 
