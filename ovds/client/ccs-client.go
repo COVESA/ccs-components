928fcd8d37727f61d31a277fa4d45ca8f99e1b5a
Magnus Gunnarsson <magnus.gunnarsson@melcogot.com>
2021-09-07 13:00:05 +0200

    changed from os.Args parsing to "github.com/akamensky/argparse" for more flexibility when using the tool

--- ovds/client/ccs-client.go
+++ ovds/client/ccs-client.go
@@ -9,13 +9,13 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
+	"flag"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"crypto/tls"
-	"crypto/x509"
-	"flag"
 	"os"
 	"strconv"
 	"strings"
@@ -23,6 +23,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/akamensky/argparse"
 	"github.com/gorilla/websocket"
 )
 
@@ -78,10 +79,10 @@ func saveListAsFile(fname string) {
 func getGen2Response(path string) string {
 	secPort := "8888"
 	scheme := "http"
-	if (secConfig.TransportSec == "yes") {
-	    scheme = "https"
-	    secPortNum, _ := strconv.Atoi(secConfig.SecPort)
-	    secPort = strconv.Itoa(secPortNum + 1)   // to diff from WSS portno
+	if secConfig.TransportSec == "yes" {
+		scheme = "https"
+		secPortNum, _ := strconv.Atoi(secConfig.SecPort)
+		secPort = strconv.Itoa(secPortNum + 1) // to diff from WSS portno
 	}
 	url := scheme + "://" + vissv2Url + ":" + secPort + pathToUrl(path)
 
@@ -94,21 +95,21 @@ func getGen2Response(path string) string {
 	// Set headers
 	req.Header.Set("Access-Control-Allow-Origin", "*")
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Host", vissv2Url + ":" + secPort)
+	req.Header.Set("Host", vissv2Url+":"+secPort)
 
 	// Configure client
 	var client *http.Client
-	if (secConfig.TransportSec == "yes") {
-	    t := &http.Transport {
-		TLSClientConfig: &tls.Config{
-		Certificates:    []tls.Certificate{clientCert},
-		RootCAs:         &caCertPool,
-	        },
-	    }
+	if secConfig.TransportSec == "yes" {
+		t := &http.Transport{
+			TLSClientConfig: &tls.Config{
+				Certificates: []tls.Certificate{clientCert},
+				RootCAs:      &caCertPool,
+			},
+		}
 
-	    client = &http.Client{Transport: t, Timeout: 10 * time.Second}	
+		client = &http.Client{Transport: t, Timeout: 10 * time.Second}
 	} else {
-	    client = &http.Client{Timeout: 10 * time.Second}
+		client = &http.Client{Timeout: 10 * time.Second}
 	}
 
 	// Send request
@@ -129,52 +130,54 @@ func getGen2Response(path string) string {
 }
 
 func writeToOVDS(message string) {
-	url := "http://" + ovdsUrl + ":8765/ovdsserver"
-	fmt.Printf("writeToOVDS: message = %s\n", message)
+	if len(ovdsUrl) != 0 {
+		url := "http://" + ovdsUrl + ":8765/ovdsserver"
+		fmt.Printf("writeToOVDS: message = %s\n", message)
 
-	path, value, timeStamp := extractMessage(message)
-	payload := `{"action":"set", "vin":"` + thisVin + `", "path":"` +  path + `", "value":"` + value + `", "timestamp":"` + timeStamp + `"}`
-//	payload := `{"action":"set", "vin":"` + thisVin + `", ` +  data[1:]
-	fmt.Printf("writeToOVDS: request payload= %s \n", payload)
+		path, value, timeStamp := extractMessage(message)
+		payload := `{"action":"set", "vin":"` + thisVin + `", "path":"` + path + `", "value":"` + value + `", "timestamp":"` + timeStamp + `"}`
+		//	payload := `{"action":"set", "vin":"` + thisVin + `", ` +  data[1:]
+		fmt.Printf("writeToOVDS: request payload= %s \n", payload)
 
-	req, err := http.NewRequest("POST", url, strings.NewReader(payload))
-	if err != nil {
-		fmt.Printf("writeToOVDS: Error creating request= %s \n", err)
-		return
-	}
-
-	// Set headers
-	req.Header.Set("Access-Control-Allow-Origin", "*")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Host", ovdsUrl+":8765")
-
-	// Set client timeout
-	client := &http.Client{Timeout: time.Second * 10}
-
-	// Send request
-	_, err = client.Do(req)
-	if err != nil {
-		fmt.Printf("writeToOVDS: Error in issuing request/response= %s ", err)
-		return
-	}
-	//	defer resp.Body.Close()
-
-	/*	body, err := ioutil.ReadAll(resp.Body)   // TODO Handle error response
+		req, err := http.NewRequest("POST", url, strings.NewReader(payload))
 		if err != nil {
-			fmt.Printf("writeToOVDS: Error in reading response= %s ", err)
+			fmt.Printf("writeToOVDS: Error creating request= %s \n", err)
 			return
-		}*/
+		}
+
+		// Set headers
+		req.Header.Set("Access-Control-Allow-Origin", "*")
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Host", ovdsUrl+":8765")
+
+		// Set client timeout
+		client := &http.Client{Timeout: time.Second * 10}
+
+		// Send request
+		_, err = client.Do(req)
+		if err != nil {
+			fmt.Printf("writeToOVDS: Error in issuing request/response= %s ", err)
+			return
+		}
+		//	defer resp.Body.Close()
+
+		/*	body, err := ioutil.ReadAll(resp.Body)   // TODO Handle error response
+			if err != nil {
+				fmt.Printf("writeToOVDS: Error in reading response= %s ", err)
+				return
+			}*/
+	}
 }
 
 func iterateGetAndWrite(elements int, sleepTime int) {
 	for i := 0; i < elements; i++ {
 		response := getGen2Response(pathList.LeafPaths[i])
-		if (len(response) == 0) {
-		    fmt.Printf("iterateGetAndWrite: Cannot connect to server.\n")
-		    os.Exit(-1)
+		if len(response) == 0 {
+			fmt.Printf("iterateGetAndWrite: Cannot connect to server.\n")
+			os.Exit(-1)
 		}
 		writeToOVDS(response)
-	        time.Sleep((time.Duration)(sleepTime) * time.Millisecond)
+		time.Sleep((time.Duration)(sleepTime) * time.Millisecond)
 	}
 	fmt.Printf("\n\n****************** Iteration cycle over all paths completed ************************************\n\n")
 }
@@ -182,15 +185,15 @@ func iterateGetAndWrite(elements int, sleepTime int) {
 func initVissV2WebSocket() *websocket.Conn {
 	scheme := "ws"
 	portNum := "8080"
-	if (secConfig.TransportSec == "yes") {
-	    scheme = "wss"
-	    portNum = secConfig.SecPort
-	    websocket.DefaultDialer.TLSClientConfig = &tls.Config{
-		Certificates:    []tls.Certificate{clientCert},
-		RootCAs:         &caCertPool,
-	        }
+	if secConfig.TransportSec == "yes" {
+		scheme = "wss"
+		portNum = secConfig.SecPort
+		websocket.DefaultDialer.TLSClientConfig = &tls.Config{
+			Certificates: []tls.Certificate{clientCert},
+			RootCAs:      &caCertPool,
+		}
 	}
-	var addr = flag.String("addr", vissv2Url + ":" + portNum, "http service address")
+	var addr = flag.String("addr", vissv2Url+":"+portNum, "http service address")
 	dataSessionUrl := url.URL{Scheme: scheme, Host: *addr, Path: ""}
 	conn, _, err := websocket.DefaultDialer.Dial(dataSessionUrl.String(), nil)
 	if err != nil {
@@ -201,43 +204,43 @@ func initVissV2WebSocket() *websocket.Conn {
 }
 
 func iterateNotificationAndWrite(conn *websocket.Conn) {
-    for {
-	_, msg, err := conn.ReadMessage()
-	if err != nil {
-	    fmt.Printf("Subscription response error: %s\n", err)
-	    return
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Printf("Subscription response error: %s\n", err)
+			return
+		}
+		message := string(msg)
+		if strings.Contains(message, "subscribe") == true {
+			fmt.Printf("Subscription response:%s\n", message)
+		} else {
+			//	    var msgMap = make(map[string]interface{})
+			//	    jsonToMap(message, &msgMap)
+			//	    data, _ := json.Marshal(msgMap["data"])
+			//	    writeToOVDS(`{"data":` + string(data) + "}")
+			writeToOVDS(message)
+		}
 	}
-	message := string(msg)
-	if (strings.Contains(message, "subscribe") == true) {
-	    fmt.Printf("Subscription response:%s\n", message)
-	} else {
-//	    var msgMap = make(map[string]interface{})
-//	    jsonToMap(message, &msgMap)
-//	    data, _ := json.Marshal(msgMap["data"])
-//	    writeToOVDS(`{"data":` + string(data) + "}")
-	    writeToOVDS(message)
-	}
-    }
 }
 
 func extractMessage(message string) (string, string, string) { // message is expected to contain the key-value: “data”:{“path”:”B”, “dp”:{“value”:”C”, “ts”:”D”}}
-    var msgMap = make(map[string]interface{})
-    jsonToMap(message, &msgMap)
-    if (msgMap["data"] == nil) {
-	fmt.Printf("Error: Message does not contain vehicle data.\n")
-        return "", "", ""
-    }
-    data, _ := json.Marshal(msgMap["data"])
+	var msgMap = make(map[string]interface{})
+	jsonToMap(message, &msgMap)
+	if msgMap["data"] == nil {
+		fmt.Printf("Error: Message does not contain vehicle data.\n")
+		return "", "", ""
+	}
+	data, _ := json.Marshal(msgMap["data"])
 
-    jsonToMap(string(data), &msgMap)
-    path := msgMap["path"].(string)
-    dp, _ := json.Marshal(msgMap["dp"])
+	jsonToMap(string(data), &msgMap)
+	path := msgMap["path"].(string)
+	dp, _ := json.Marshal(msgMap["dp"])
 
-    jsonToMap(string(dp), &msgMap)
-    value := msgMap["value"].(string)
-    ts := msgMap["ts"].(string)
-fmt.Printf("path=%s, value=%s, ts=%s\n", path, value, ts)
-    return path, value, ts
+	jsonToMap(string(dp), &msgMap)
+	value := msgMap["value"].(string)
+	ts := msgMap["ts"].(string)
+	fmt.Printf("path=%s, value=%s, ts=%s\n", path, value, ts)
+	return path, value, ts
 }
 
 func jsonToMap(request string, rMap *map[string]interface{}) {
@@ -251,38 +254,67 @@ func jsonToMap(request string, rMap *map[string]interface{}) {
 func subscribeToPaths(conn *websocket.Conn, elements int, sleepTime int) {
 	for i := 0; i < elements; i++ {
 		subscribeToPath(conn, pathList.LeafPaths[i])
-	        time.Sleep((time.Duration)(sleepTime) * time.Millisecond)
+		time.Sleep((time.Duration)(sleepTime) * time.Millisecond)
 	}
 }
 
 func subscribeToPath(conn *websocket.Conn, path string) {
-    request := `{"action":"subscribe", "path":"` + path + `", "filter":{"op-type":"capture", "op-value":"time-based", "op-extra":{"period":"3"}}, "requestId": "6578"}`
+	request := `{"action":"subscribe", "path":"` + path + `", "filter":{"op-type":"capture", "op-value":"time-based", "op-extra":{"period":"3"}}, "requestId": "6578"}`
 
-    err := conn.WriteMessage(websocket.TextMessage, []byte(request))
-    if err != nil {
-	fmt.Printf("Subscribe request error:%s\n", err)
-    }
+	err := conn.WriteMessage(websocket.TextMessage, []byte(request))
+	if err != nil {
+		fmt.Printf("Subscribe request error:%s\n", err)
+	}
 
 }
 
 func transferData(elements int, sleepTime int, accessMode string) {
-	if (accessMode == "get") {
-	    for {
-		iterateGetAndWrite(elements, sleepTime)
-	    }
+	if accessMode == "get" {
+		for {
+			iterateGetAndWrite(elements, sleepTime)
+		}
 	} else {
-	    conn := initVissV2WebSocket()
-	    go iterateNotificationAndWrite(conn)
-	    subscribeToPaths(conn, elements, sleepTime)
-	    for {
-	        time.Sleep(1000 * time.Millisecond)  // just to keep alive...
-	    }
+		conn := initVissV2WebSocket()
+		go iterateNotificationAndWrite(conn)
+		subscribeToPaths(conn, elements, sleepTime)
+		for {
+			time.Sleep(1000 * time.Millisecond) // just to keep alive...
+		}
 	}
 }
 
 func main() {
+	// Create new parser object
+	parser := argparse.NewParser("print", "Prints provided string to stdout")
 
-	if len(os.Args) != 6 {
+	// Create flags
+	url_viss := parser.String("w", "vissv2Url", &argparse.Options{Required: true, Help: "IP/URL to W3C VISS v2 server (REQUIRED)"})
+	url_ovds := parser.String("o", "ovdsUrl", &argparse.Options{Required: false, Help: "IP/url to OVDS server"})
+	vin := parser.String("v", "vin", &argparse.Options{Required: false, Help: "VIN number", Default: "ULF001"})
+	iter := parser.Int("p", "iterationPeriod", &argparse.Options{Required: false, Help: "estimated time in msec for one roundtrip", Default: 15})
+	accMode := parser.Selector("a", "accessMode", []string{"get", "subscribe"}, &argparse.Options{
+		Required: false,
+		Help:     "CCS client access-mode must be either get or subscribe.",
+		Default:  "get"})
+
+	//	accessMode := parser.String("a", "accessMode", &argparse.Options{Required: true, Help: "CCS client access-mode must be either get or subscribe."})
+
+	// Parse input
+	err := parser.Parse(os.Args)
+	if err != nil {
+		// In case of error print error and print usage
+		// This can also be done by passing -h or --help flags
+		fmt.Print(parser.Usage(err))
+	}
+
+	//conversion since consts are defined as string type and not *string
+	vissv2Url = *url_viss
+	ovdsUrl = *url_ovds
+	thisVin = *vin
+	iterationPeriod := *iter
+	accessMode := *accMode
+
+	/**if len(os.Args) != 6 {
 		fmt.Printf("CCS client command line: ./client gen2-server-url OVDS-server-url vin list-iteration-time access-mode\n")
 		os.Exit(1)
 	}
@@ -295,24 +327,25 @@ func main() {
 		fmt.Printf("CCS client access-mode must be either get or subscribe.\n")
 		os.Exit(1)
 	}
+	*/
 
 	readTransportSecConfig()
-        fmt.Printf("InitClientServer():secConfig.TransportSec=%s", secConfig.TransportSec)
-	if (secConfig.TransportSec == "yes") {
-	    caCertPool = *prepareTransportSecConfig()
+	fmt.Printf("InitClientServer():secConfig.TransportSec=%s", secConfig.TransportSec)
+	if secConfig.TransportSec == "yes" {
+		caCertPool = *prepareTransportSecConfig()
 	}
 
 	if createListFromFile("vsspathlist.json") == 0 {
-	    if createListFromFile("../vsspathlist.json") == 0 {
-		fmt.Printf("Failed in creating list from vsspathlist.json\n")
-		os.Exit(1)
-	    }
+		if createListFromFile("../vsspathlist.json") == 0 {
+			fmt.Printf("Failed in creating list from vsspathlist.json\n")
+			os.Exit(1)
+		}
 	}
 
 	elements := len(pathList.LeafPaths)
-	sleepTime := (iterationPeriod*1000-elements*30)/elements  // 30 = estimated time in msec for one roundtrip - get data from VISSv2 server, write data to OVDS
-	if (sleepTime < 1) {
-	    sleepTime = 1
+	sleepTime := (iterationPeriod*1000 - elements*30) / elements // 30 = estimated time in msec for one roundtrip - get data from VISSv2 server, write data to OVDS
+	if sleepTime < 1 {
+		sleepTime = 1
 	}
 	transferData(elements, sleepTime, accessMode)
 }
